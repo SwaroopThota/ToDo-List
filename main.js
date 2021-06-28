719983b8cf5b043929de3c20977f754decd75740
SwaroopThota <swaroopthota2001@gmail.com>
2021-06-28 13:45:32 +0530

    fdsfsdf

--- main.js
+++ main.js
@@ -1,69 +1,61 @@
-function addEvent() {
-  let title = document.getElementById("title").value;
-  if (title.length > 0) {
-    if (localStorage.getItem("listJson") == null) {
-      list = [];
-      list.push([title]);
-      localStorage.setItem("listJson", JSON.stringify(list));
-    } else {
+function addTask() {
+  let input = document.getElementById("title");
+  if (input.value.length > 0) {
+    list = [];
+    if (localStorage.getItem("listJson") != null){
       listStr = localStorage.getItem("listJson");
       list = JSON.parse(listStr);
-      list.push([title]);
-      localStorage.setItem("listJson", JSON.stringify(list));
     }
-    document.getElementById("title").value = "";
+    list.push({
+      name: input.value,
+      completed:false
+    });
+    localStorage.setItem("listJson", JSON.stringify(list));
+    input.value = "";
     updateList();
   }
 }
-
 function updateList() {
   if (localStorage.getItem("listJson") != null) {
     listStr = localStorage.getItem("listJson");
     list = JSON.parse(listStr);
-    let str = "";
+    let str = '<li class="list-group-item active fs-4 text-center" aria-current="true">Tasks</li>';
+    if(list.length==0){
+      str+='<li class="list-group-item text-muted">No tasks to do.</li>';
+    }else{
     list.forEach((element, index) => {
-      str += `
-        <tr onclick="done(this)">
-        <th scope="row" class="user-select-none w-25">${index + 1}</th>
-        <td class="user-select-none w-50 text-break">${element}</td>
-        <td class="user-select-none"><button class="btn btn-primary mx-2" onclick="deleteEvent(${index})">Delete</button></td>
-      </tr>
-        `;
+      str += `<li class="list-group-item list-group-item-action user-select-none`;
+      str += (element.completed)? ` text-decoration-line-through`:` `;
+      str +=`"  onclick="done(${index})" >${index+1}. ${element.name}</li>`;
     });
-    document.getElementById("tableBody").innerHTML = str;
+  }
+    document.getElementById("taskList").innerHTML= str;
   }
 }
-
-function deleteEvent(index) {
-  console.log("deleting");
+function done(index) {
   if (localStorage.getItem("listJson") != null) {
     listStr = localStorage.getItem("listJson");
     list = JSON.parse(listStr);
-    list.splice(index, 1);
+    list[index].completed=!list[index].completed;
     localStorage.setItem("listJson", JSON.stringify(list));
-    updateList();
   }
-}
-
-function clearList() {
-  if (localStorage.getItem("listJson") != null) {
-    listStr = localStorage.getItem("listJson");
-    list = JSON.parse(listStr);
-    if (list.length != 0) {
-      if (confirm("Do you want to clear the list?")) {
-        list.splice(0, list.length);
-        localStorage.setItem("listJson", JSON.stringify(list));
-        updateList();
-      }
-    }
-  }
-}
-function done(tr) {
-  tr.classList.toggle("text-decoration-line-through");
+  updateList();
 }
 function onEnter(event) {
   if (event.keyCode === 13) {
     event.preventDefault();
     document.getElementById("addTask").click();
+  }
+}
+function clearCompleted(){
+  if (localStorage.getItem("listJson") != null) {
+    listStr = localStorage.getItem("listJson");
+    list = JSON.parse(listStr);
+    list=list.filter(function (ele){
+      if(!ele.completed)
+      return ele;
+    });
+    localStorage.setItem("listJson", JSON.stringify(list));
+    updateList();
   }
 }
